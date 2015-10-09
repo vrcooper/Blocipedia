69dd7afbc5c1ec739fa844624a885f1467c8042a
Virginia <vcooper_us@yahoo.com>
2015-10-09 15:38:20 -0700

    added mailgun app.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,17 @@
 if Rails.env.development? || Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
+  
   ActionMailer::Base.smtp_settings = {
-    address:         'smtp.sendgrid.net',
     port:            '587',
+    address:         'smtp.mailgun.org',
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         'vrcooper-blocipedia.herokuapp.com',
     authentication:  :plain,
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
-    domain:         'heroku.com',
-    enable_starttls_auto: true
+    content_type:    'text/html'
   }
+
+  ActionMailer::Base.delivery_method = :smtp
+
+  # Makes debugging *way* easier.
+  ActionMailer::Base.raise_delivery_errors = true
 end

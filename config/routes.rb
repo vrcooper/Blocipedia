a3a25c89a568191e97d938c008b5af5b25985491
Virginia <vcooper_us@yahoo.com>
2015-10-13 11:26:18 -0700

    First unsuccessful attempt at setting up wiki crud.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
+  
+  get 'wikis/create'
+
   devise_for :users
+  resources :users, only: [:show] do
+    resources :wikis, only: [:create, :destroy]
+  end 
+
   get 'welcome/index'
 
-   root to: 'welcome#index'
+  authenticated do
+
+    root to: 'users#show', as: :authenticated_root
 
   end
+
+  unauthenticated do
+  
+   root to: 'welcome#index'
+
+ end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+uses = User.all
+
+# Create Wikis
+
+10.times do 
+  Wiki.create!(
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+
+    )
+  
+end
+wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
